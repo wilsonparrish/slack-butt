acdec4b0a1199c4925a36bd61209ec02a4ea9bea
Wilson Parrish <wilsonparrishmk2@gmail.com>
2016-01-12 11:50:13 -0600

    changing Mongo creds

--- core/server/config/env/development.js
+++ core/server/config/env/development.js
@@ -2,6 +2,6 @@
 module.exports = {
     // you want to get your own mongolab database and credentials from within Heroku
     // if you leave it like this you will be hitting my database :)
-    db: 'mongodb://wpmk2:madcatmk3@ds035563.mongolab.com:35563/slackbutt',
+    db: 'mongodb://wpmk2:madcatmk3@ds035563.mongolab.com:35563/slack-butt',
     sessionSecret: 'developmentSessionSecret'
 };
