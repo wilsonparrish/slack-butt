d562f8922838e5837f60d80d277a68daa092e691
Wilson Parrish <wilsonparrishmk2@gmail.com>
2016-01-12 10:19:05 -0600

    initial slack butt

---
+++ core/server/config/env/production.js
@@ -0,0 +1,7 @@
+// ENVIRONMENT VARIABLES
+module.exports = {
+    // you want to get your own mongolab database and credentials
+    // if you leave it like this you will be hitting my database :)
+    db: 'mongodb://wpmk2:madcatmk3@ds035563.mongolab.com:35563/slackbutt',
+    sessionSecret: 'productionSessionSecret'
+};
